7e84e9f3d40c5e473424989afe8ee9ba9c04837a
johnsun <wlqsun@163.com>
2017-02-09 18:21:41 +0800

    update common header files

--- common/macros.h
+++ common/macros.h
@@ -1,11 +1,22 @@
 
-#ifndef TCPRELAY_UTILS_MACROS_H_
-#define TCPRELAY_UTILS_MACROS_H_
+#ifndef COMMON_MACROS_H_
+#define COMMON_MACROS_H_
+
+#ifndef __GNUC__
+#error Please use compiler gcc to compile
+#endif
 
 #include <stddef.h>  // For size_t.
 #include <string.h>  // For memcpy.
 
 #include <errno.h>  // For errno.
+
+// https://gcc.gnu.org/onlinedocs/gcc/Alternate-Keywords.html#Alternate-Keywords
+// The keywords asm, typeof and inline are not available in programs compiled
+// with -ansi or -std.
+#define asm __asm__
+#define typeof __typeof__
+#define inline __inline__
 
 #define ALLOW_UNUSED __attribute__((unused))
 
@@ -41,21 +52,29 @@ char(&ArraySizeHelper(T(&array)[N]))[N];
     eintr_wrapper_result;                                 \
   })
 
-#define ALIGNOF(type) __alignof__(type)
-
-// https://gcc.gnu.org/onlinedocs/gcc-5.1.0/gcc/Type-Attributes.html
-// The aligned attribute can only increase the alignment;
-// but you can decrease it by specifying packed as well.
-#define ALIGNAS(byte_alignment) __attribute__((aligned(byte_alignment)))
-
-#define PACKED __attribute__((packed))
-
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
-
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&);               \
   void operator=(const TypeName&)
 
+#define ALIGNOF(type) __alignof__(type)
+
+// https://gcc.gnu.org/onlinedocs/gcc-5.1.0/gcc/Type-Attributes.html
+// The aligned attribute can only increase the alignment;
+// but you can decrease it by specifying packed as well.
+#define ALIGNAS(byte_alignment) __attribute__((aligned(byte_alignment)))
+#define PACKED __attribute__((packed))
+
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+#if __GNUC__ >= 4
+#define DLL_PUBLIC __attribute__((visibility("default")))
+#define DLL_LOCAL __attribute__((visibility("hidden")))
+#else
+#define DLL_PUBLIC
+#define DLL_LOCAL
 #endif
+
+#endif  // COMMON_MACROS_H_
