b7cf1aab9d2c791d4ab1eeee35e92f6894193cc4
johnsun <wlqsun@163.com>
2017-02-09 14:24:07 +0800

    add `DISALLOW_COPY_AND_ASSIGN`

--- common/macros.h
+++ common/macros.h
@@ -52,4 +52,10 @@ char(&ArraySizeHelper(T(&array)[N]))[N];
 
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
+// A macro to disallow the copy constructor and operator= functions
+// This should be used in the private: declarations for a class
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
 #endif
