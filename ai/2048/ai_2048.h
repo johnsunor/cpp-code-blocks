3874bdc4c3bab36b7b210140421cba47e04ecafc
johnsun <wlqsun@163.com>
2017-04-20 15:39:15 +0800

    update ai-2048

--- ai/2048/ai_2048.h
+++ ai/2048/ai_2048.h
@@ -14,67 +14,64 @@ namespace ai_set {
 
 class AI2048 {
  public:
-   typedef ::std::pair<int,int> PosPair;
+  typedef ::std::pair<int, int> PosPair;
 
-   AI2048() {
-     Clear();
-   }
+  AI2048() { Clear(); }
 
-   void CopyTilesFrom(const AI2048& ai) {
-     if (this != &ai) {
-       memcpy(tiles_, ai.tiles_, sizeof(tiles_));
-     }
-   }
+  void CopyTilesFrom(const AI2048& ai) {
+    if (this != &ai) {
+      memcpy(tiles_, ai.tiles_, sizeof(tiles_));
+    }
+  }
 
-   void AddComputerMove(int grid[4][4]);
-   
-   int GetPlayerBestMove();
+  void AddComputerMove(int grid[4][4]);
 
-   void Print(const char* msg=NULL) const;
-   void Clear();
+  int GetPlayerBestMove();
 
-   void GetAllTiles(int grid[4][4]);
+  void Print(const char* msg = NULL) const;
+  void Clear();
 
-   int GetAllEmptyPos(int pos_set[]) const;
+  void GetAllTiles(int grid[4][4]);
 
-   void AddRandTile();
+  int GetAllEmptyPos(int pos_set[]) const;
 
-   int GetCandidateSet(int candidate_pos_set[],
-                       int candidate_val_set[]);
+  void AddRandTile();
 
-   bool IsValid(int pos) const;
+  int GetCandidateSet(int candidate_pos_set[], int candidate_val_set[]);
 
-   void Mark(int pos, int val, bool marked[20]) const;
-   
-   PosPair GetTargetPos(int pos, int dir) const; 
+  bool IsValid(int pos) const;
 
-   int EvalSmoothnessValue() const;
+  void Mark(int pos, int val, bool marked[20]) const;
 
-   int GetMaxValue() const;
+  PosPair GetTargetPos(int pos, int dir) const;
 
-   int GetLandNum() const;
+  int EvalSmoothnessValue() const;
 
-   int GetEmptyPosNum() const;
+  int GetMaxValue() const;
 
-   int  EvalMonotonicityValue() const;
+  int GetLandNum() const;
 
-   double EvalAll() const;
-  
-   bool IsWin() const;
+  int GetEmptyPosNum() const;
 
-   bool TryMove(int dir);
-   
-   double MiniMaxSearch(int depth, bool is_player, double alpha, double beta);
+  int EvalMonotonicityValue() const;
+
+  double EvalAll() const;
+
+  bool IsWin() const;
+
+  bool TryMove(int dir);
+
+  double MiniMaxSearch(int depth, bool is_player, double alpha, double beta);
 
  private:
-   AI2048(const AI2048&);
-   AI2048& operator=(const AI2048&);
+  AI2048(const AI2048&);
+  AI2048& operator=(const AI2048&);
 
  private:
-   int8_t tiles_[20];
-   int best_move_;
+  int8_t tiles_[20];
+  int best_move_;
 };
 
-} //namespace ai_set
+}  // namespace ai_set
 
-#endif //AI2048_H
+#endif  // AI2048_H
--- ai/2048/ai_utils.h
+++ ai/2048/ai_utils.h
@@ -1,7 +1,7 @@
 #ifndef MY_UTILS_H
 #define MY_UTILS_H
 
-//C89
+// C89
 #include <cstdio>
 #include <cctype>
 #include <cstring>
@@ -10,10 +10,10 @@
 #include <ctime>
 #include <cassert>
 
-//C99
+// C99
 #include <stdint.h>
 
-//C++98
+// C++98
 #include <iostream>
 #include <string>
 #include <vector>
@@ -25,19 +25,19 @@
 #include <algorithm>
 #include <utility>
 
-//POSIX
+// POSIX
 #include <unistd.h>
 
-//MACRO
+// MACRO
 #define INF 1000
 
 namespace ai_utils {
 
 const int offset[] = {
-    -4, //UP
-    1,  //RIGHT
-    4,  //DOWN
-    -1  //LEFT
+    -4,  // UP
+    1,   // RIGHT
+    4,   // DOWN
+    -1   // LEFT
 };
 
 const int kUp = 0;
@@ -46,68 +46,63 @@ const int kDown = 2;
 const int kLeft = 3;
 
 const int dir_pos[16][4] = {
-    { -1, 1,  4,  -1 },
-    { -1, 2,  5,  0  },
-    { -1, 3,  6,  1  },
-    { -1, -1, 7,  2  },
-    { 0,  5,  8,  -1 },
-    { 1,  6,  9,  4  },
-    { 2,  7,  10, 5  },
-    { 3,  -1, 11, 6  },
-    { 4,  9,  12, -1 },
-    { 5,  10, 13, 8  },
-    { 6,  11, 14, 9  },
-    { 7,  -1, 15, 10 },
-    { 8,  13, -1, -1 },
-    { 9,  14, -1, 12 },
-    { 10, 15, -1, 13 },
-    { 11, -1, -1, 14 },
+    {-1, 1, 4, -1},
+    {-1, 2, 5, 0},
+    {-1, 3, 6, 1},
+    {-1, -1, 7, 2},
+    {0, 5, 8, -1},
+    {1, 6, 9, 4},
+    {2, 7, 10, 5},
+    {3, -1, 11, 6},
+    {4, 9, 12, -1},
+    {5, 10, 13, 8},
+    {6, 11, 14, 9},
+    {7, -1, 15, 10},
+    {8, 13, -1, -1},
+    {9, 14, -1, 12},
+    {10, 15, -1, 13},
+    {11, -1, -1, 14},
 };
 
-const double kSmoothWeight   =  0.1;
-const double kMonoWeight     =  1.0;
-const double kEmptyWeight    =  2.7;
-const double kMaxValueWeight =  1.0;
+const double kSmoothWeight = 0.1;
+const double kMonoWeight = 1.0;
+const double kEmptyWeight = 2.7;
+const double kMaxValueWeight = 1.0;
 
-inline int MkPos(int x, int y) {
-    return x * 4 + y;
-}
+inline int MkPos(int x, int y) { return x * 4 + y; }
 
-inline int X(int pos) {
-    return pos / 4;
-}
+inline int X(int pos) { return pos / 4; }
 
-inline int Y(int pos) {
-    return pos % 4;
-}
+inline int Y(int pos) { return pos % 4; }
 
 inline ::std::size_t lg2(::std::size_t n) {
-    if (n == 0) {
-      printf("%u\n", static_cast<unsigned>(n));
-      return 0;
-    }
-    assert(n > 0);
-    ::std::size_t k = 0;
-    for (; n > 1; n >>= 1) ++k;
-    return k;
+  if (n == 0) {
+    printf("%u\n", static_cast<unsigned>(n));
+    return 0;
+  }
+  assert(n > 0);
+  ::std::size_t k = 0;
+  for (; n > 1; n >>= 1) ++k;
+  return k;
 }
 
 inline bool TimeOut(clock_t start_time, ::std::size_t msec) {
-    return (clock() - start_time > static_cast<double>(msec)/1000.0f * CLOCKS_PER_SEC);
+  return (clock() - start_time >
+          static_cast<double>(msec) / 1000.0f * CLOCKS_PER_SEC);
 }
 
 template <typename T>
 inline T ArrayMax(T array[], ::std::size_t array_size) {
-    assert(array_size > 0);
-    T max_val = array[0];
-    for (::std::size_t i = 1; i < array_size; ++i) {
-        if (max_val < array[i]) {
-            max_val = array[i];
-        }
+  assert(array_size > 0);
+  T max_val = array[0];
+  for (::std::size_t i = 1; i < array_size; ++i) {
+    if (max_val < array[i]) {
+      max_val = array[i];
     }
-    return max_val;
+  }
+  return max_val;
 }
 
-} // namespace ai_utils
+}  // namespace ai_utils
 
-#endif // MY_UTILS_H
+#endif  // MY_UTILS_H
